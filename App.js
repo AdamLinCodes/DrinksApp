c061c8db0d915bc6e916cd852b12557c01b39ad7
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-05-30 23:57:40 -0400

    adding page navigation

--- App.js
+++ App.js
@@ -1,44 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import CustomButton from './components/button.component';
-import Title from './components/title.component';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+import MainPage from './components/pages.component';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Title/>
-      
-      <View style={styles.buttonContainer}>
-        <CustomButton title="Drinks"/>
-        <CustomButton title="Drinks"/>
-      </View>
-      
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainPage} />
+        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    height: screenHeight,
-    width: screenWidth,
-    backgroundColor: '#E0FFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-    // borderWidth: 2, // Border width in pixels
-    // borderColor: 'red',
-  },
-  buttonContainer: {
-    height: screenHeight * 0.1,
-    width: screenWidth * 0.8,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    // borderWidth: 2, // Border width in pixels
-    // borderColor: 'green',
-  }
-});
+export default App;
